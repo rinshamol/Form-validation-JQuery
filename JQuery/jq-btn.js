9a1b99ea478e10ac8ca2b780139924384c93ea3a
rinshamol <rinshamolks65@gmail.com>
2024-09-24 12:31:46 +0530

    final commit

---
+++ JQuery/jq-btn.js
@@ -0,0 +1,43 @@
+function validation(){
+    let isValid = true;
+    if($('#name').val()==''){
+        $('#er1').show();
+        isValid=false;
+    }else{
+       $('#er1').hide();
+    }
+    if($('#email').val()==''){
+        $('#er2').show();
+        isValid=false;
+    }else{
+       $('#er2').hide();
+    }
+    if($('#password').val()==''){
+        $('#er3').show();
+        isValid=false;
+    }else{
+        $('#er3').hide();
+    }
+    if($('#address').val()==''){
+        $('#er4').show();
+        isValid=false;
+    }else{
+        $('#er4').hide();
+    }
+    if(!$('input[name="gender"]:checked').length){
+        $('#er5').show();
+        isValid=false;
+    }else{
+        $('#er5').hide();
+    }
+    if(!$('input[name="check"]:checked').length){
+        $('#er6').show();
+        isValid=false;
+    }else{
+        $('#er6').hide();
+    }
+    if(isValid){
+        $('#myForm').submit();
+        alert("Form Submitted Successfully!!");
+    }
+}
